ecf2f343bb513348465428b02fef7975307157f4
wwhitcomb <wwhitcomb2001@hotmail.com>
2016-12-12 18:39:54 -0700

    upload final version

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ function getTokens(rawString) {
 	return rawString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
 }
 
+function getTokensSent(rawString) {
+	return rawString.toLowerCase().split(/!.?/).filter(Boolean).sort();
+}
+
 function GetUniqueWordCount(textArray) {
 	var uniqueWords = [];
 	for (var i = 0; i < textArray.length; i++) {
@@ -25,22 +29,34 @@ function GetUniqueWordCount(textArray) {
 }
 
 function GetAvgWordLength(textArray) {
+	var charTotal = 0;
+	for(var i = 0; i < textArray.length; i++) {
+		charTotal += textArray[i].length;
+	}
 	
+	var finalResult = (charTotal / textArray.length); 
+	return finalResult.toFixed(2);
 }
 
-function GetAvgSentenceLength(textArray) {
+function GetAvgSentenceLength(sentArray) {
+	var sentTotal = 0;
+	for(var i = 0; i < sentArray.length; i++) {
+		sentTotal += sentArray[i].length;
+	}
 	
+	var finalResult = (sentTotal / sentArray.length); 
+	return finalResult.toFixed(2);
 }
 
 $(document).ready(function () {
 	$(".js-text-form").submit(function (event) {
 		event.preventDefault();
 		var textArray = getTokens($('#user-text').val());
+		var sentArray = getTokensSent($('#user-text').val());
 		$(".wordcount").text(textArray.length);
 		$(".uniquewordcount").text(GetUniqueWordCount(textArray));
 		$(".avgwordlength").text(GetAvgWordLength(textArray));
-		$(".avgsentencelength").text(GetAvgSentenceLength(text));
-		$(".text-report").removeClass(".hidden");
+		$(".avgsentencelength").text(GetAvgSentenceLength(sentArray));
+		$(".text-report").removeClass("hidden");
 	});
 });
-
